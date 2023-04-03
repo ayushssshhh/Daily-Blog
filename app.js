127fc00ccefc03621626b0d09c2beeb843b229cc
ayushssshhh <ayushssshhh@gmail.com>
2023-04-03 14:24:37 +0530

    v1.1.0 mongo connectd

--- app.js
+++ app.js
@@ -5,80 +5,132 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 // Load the full build.
 const _ = require('lodash');
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const mongoose = require("mongoose");
+
+// connecting to mongodb
+async function run() {
+  // await mongoose.connect('mongodb+srv://ayushssshhh:MongoDBisLove@cluster0.wbywpat.mongodb.net/?retryWrites=true&w=majority/DailyBlog', { useUnifiedTopology: true, useNewUrlParser: true });
+  await mongoose.connect('mongodb://127.0.0.1:27017/DailyBlog', { useUnifiedTopology: true, useNewUrlParser: true });
+  mongoose.connection.once('open', () => {
+    console.log('Connected to db');
+  });
+}
 
 const app = express();
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+// blogSchema
+const blogSchema = {
+  title: {
+    type: String,
+    required: ["true", "You havent specified Title of your blog"]
+  },
+  body: String,
+  img: String
+}
+
+// creating Blog collection
+const Blog = mongoose.model("Blog", blogSchema);
+
 
 
 const port = 5000;
-let posts = [];
 
-let blog1 = {
-  title : "Daily Blog",
-  body : "DailyBlog is an online bloging website made by Kumar Ayush as a personal development project. This website is a dynamic website which enables user to post their blogs through compose page and get listed on home page, users can also read posted Blogs in home page by clicking on it.<br>Technologies used : <br><br>&emsp;&emsp; &emsp;&emsp; 1. html <br>&emsp; &emsp;&emsp; &emsp;  2. CSS <br>&emsp; &emsp;&emsp; &emsp;  3. JavaScript <br>&emsp;&emsp; &emsp; &emsp;  4. NodeJs <br>&emsp;&emsp; &emsp; &emsp;  5. Express <br>&emsp;&emsp; &emsp; &emsp;  6. EJS <br>&emsp;&emsp; &emsp; &emsp;  7. Bootstrap",
-  img : "/images/blog1.png"
-}
+const blog1 = new Blog({
+  title: "Daily Blog",
+  body: "DailyBlog is an online bloging website made by Kumar Ayush as a personal development project. This website is a dynamic website which enables user to post their blogs through compose page and get listed on home page, users can also read posted Blogs in home page by clicking on it.<br>Technologies used : <br><br>&emsp;&emsp; &emsp;&emsp; 1. html <br>&emsp; &emsp;&emsp; &emsp;  2. CSS <br>&emsp; &emsp;&emsp; &emsp;  3. JavaScript <br>&emsp;&emsp; &emsp; &emsp;  4. NodeJs <br>&emsp;&emsp; &emsp; &emsp;  5. Express <br>&emsp;&emsp; &emsp; &emsp;  6. EJS <br>&emsp;&emsp; &emsp; &emsp;  7. Bootstrap",
+  img: "/images/blog1.png"
+});
 
-posts.push(blog1);
+app.get("/", async (req, res) => {
+  run();
+  // to get count of records in Blog
+  await Blog.count().then((count, err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (count === 0)
+        blog1.save();
+    }
+  })
 
-app.get("/",(req,res)=>{
-  res.render("home" ,{
-    postsRender : posts,
+  await Blog.find().then((post, err) => {
+    if (err) {
+      console.log("----err occured-----\n" + err);
+    }
+    res.render("home", {
+      postsRender: post,
+    });
   })
 
 })
 
-app.get("/about",(req,res)=>{
+app.get("/about", (req, res) => {
   res.render("about");
-})
+});
 
-app.get("/contact",(req,res)=>{
-  res.render("contact" ,{
-    contactContentRender : contactContent
-  })
-})
-
-app.get("/compose" , (req , res)=>{
+app.get("/compose", (req, res) => {
   res.render("compose");
 })
 
-app.get("/post/:topic" , (req , res)=>{
+app.get("/post/:topic", async (req, res) => {
   let topic = req.params.topic;
   topic = _.lowerCase(topic);
 
-
-  posts.forEach((posts)=>{
-
-    let title = posts.title;
-    title = _.lowerCase(title);
-    
-    if(title === topic){
-      res.render("post" , {
-        mainPostRender : posts       
-      });
+  await Blog.findOne({ title: topic }).then((post, err) => {
+    if (post == null) {
+      res.render("post", {
+        mainPostRender: blog1
+      })
+    } else {
+      res.render("post", {
+        mainPostRender: post
+      })
     }
+
   })
+
+
+  // posts.forEach((posts)=>{
+
+  //   let title = posts.title;
+  //   title = _.lowerCase(title);
+
+  //   if(title === topic){
+  //     res.render("post" , {
+  //       mainPostRender : posts       
+  //     });
+  //   }
+  // })
 })
 
-app.post("/compose",(req , res)=>{
-  let post = {
-    title : req.body.inputTitle,
-    body : req.body.inputPost,
-    img : req.body.inputImgUrl
-  }
+app.post("/compose", (req, res) => {
+  let post = new Blog({
+    title: req.body.inputTitle,
+    body: req.body.inputPost,
+    img: req.body.inputImgUrl
+  })
 
-  posts.push(post);
+  post.save();
+
   res.redirect("/");
 })
 
 
 
+// contact page
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-app.listen(process.env.PORT || port, ()=>{
+app.get("/contact", (req, res) => {
+  res.render("contact", {
+    contactContentRender: contactContent
+  })
+})
+
+app.listen(process.env.PORT || port, () => {
   console.log("server started : " + port);
 })
 
